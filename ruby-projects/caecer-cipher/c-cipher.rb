fbb0a19fc1d3c8fb5f7ab789c5fa3b4c8ca52cee
Daniel Samuel <dansam5000@gmail.com>
2022-10-13 18:43:29 +0100

    Caecer-cipher soln implementation method-2

--- ruby-projects/caecer-cipher/c-cipher.rb
+++ ruby-projects/caecer-cipher/c-cipher.rb
@@ -23,3 +23,22 @@ end
 cipher = CaecerCipher.new
 p cipher.encode("What a string!", 5)
 
+# Method 2
+def caecer_cipher_2(string, key)
+    alphabet = ('a'..'z').to_a
+    alphabet_2 = ('A'..'Z').to_a
+    string.chars.map do |char|
+        if alphabet.include?(char)
+            index = alphabet.index(char)
+            alphabet[(index + key) % alphabet.size]
+        elsif alphabet_2.include?(char)
+            index = alphabet_2.index(char)
+            alphabet_2[(index + key) % alphabet_2.size]
+        else
+            char
+        end
+    end.join
+end
+
+p caecer_cipher_2("What a string!", 5)
+
