6a9cfecf81c0535b4ad235a1b770a43c3d1c9db3
Daniel Samuel <dansam5000@gmail.com>
2022-10-27 16:05:33 +0100

    iteration0: Loading a file

--- serialization/event-manager/lib/event_manager.rb
+++ serialization/event-manager/lib/event_manager.rb
@@ -1,1 +1,13 @@
 puts "Event Manager Initialized!"
+
+
+contents = File.read('event_attendees.csv')
+p contents[3]
+
+lines = File.readlines("event_attendees.csv")
+lines.each do |line|
+  next if line == lines[0]
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
