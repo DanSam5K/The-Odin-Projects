8471da6c546d4ecbc9b179014be6f8d8a89e183d
Daniel Samuel <dansam5000@gmail.com>
2022-10-28 15:56:22 +0100

    Iteration4: form letters

--- serialization/event-manager/lib/event_manager.rb
+++ serialization/event-manager/lib/event_manager.rb
@@ -1,10 +1,23 @@
 require "csv"
 require "google/apis/civicinfo_v2"
+require "erb"
 
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
+
+def clean_phone_number(phone_number)
+  phone_number = phone_number.to_s.gsub(/[^0-9]/, "")
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == "1"
+    phone_number[1..-1]
+  else
+    "0000000000"
+  end
+end
+
 
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -15,10 +28,8 @@ def legislators_by_zipcode(zipcode)
       address: zipcode,
       levels: "country",
       roles: ["legislatorUpperBody", "legislatorLowerBody"]
-    )
-    legislators = legislators.officials
+    ).officials
     
-    legislator_names = legislators.map(&:name).join(", ")
   rescue
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end 
@@ -26,13 +37,22 @@ end
 
 puts "Event Manager Initialized!"
 
+template_letter = File.read("form_letter.html")
+
 contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
+
   legislator_names = legislators_by_zipcode(zipcode)
-
-
-  puts "#{name} #{zipcode} #{legislator_names}"
+  
+  form_letter = erb_template.result(binding)
+   
+  save_thank_you_letters(id, form_letter)
 end
