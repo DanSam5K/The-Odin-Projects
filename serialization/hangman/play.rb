8764d11e8f7b3b205e0516a6640f4da0f1d75abc
Daniel Samuel <dansam5000@gmail.com>
2022-11-04 11:20:15 +0100

    Update word with google word txt

--- serialization/hangman/play.rb
+++ serialization/hangman/play.rb
@@ -1,22 +1,27 @@
+require 'open-uri'
+
 class Hangman
   def initialize
     @word = words.sample
     @lives = 7
     @word_teaser = ""
 
-    @word.first.size.times do
+    @word.size.times do
       @word_teaser += "_ "
     end
   end
 
+  def download_google_words
+    url = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+
+    words_file = URI.open(url).read
+    words_file_array = words_file.split("\n")
+
+    words_file_array
+  end
+
   def words
-    [
-      ["cricket", "A game played by two gentlemen"],
-      ["jogging", "We are not walking..."],
-      ["celebrate", "Remembering special moments"],
-      ["continent", "There are 7 of these"],
-      ["exotic", "Not from around here..."],
-    ]
+    words = download_google_words
   end
 
   def print_teaser(last_guess = nil)
@@ -28,7 +33,7 @@ class Hangman
     new_teaser = @word_teaser.split
 
     new_teaser.each_with_index do |letter, index|
-      if letter == '_' && @word.first[index] == last_guess
+      if letter == '_' && @word[index] == last_guess
         new_teaser[index] = last_guess
       end
     end
@@ -41,14 +46,14 @@ class Hangman
       puts "Enter a letter"
       guess = gets.chomp
 
-      good_guess = @word.first.include? guess
+      good_guess = @word.include? guess
       if guess == "exit"
         puts "Thank you for playing!"
       elsif good_guess
         puts "You are correct!"
 
         print_teaser(guess)
-        if @word.first == @word_teaser.split.join
+        if @word == @word_teaser.split.join
           puts "Congratulations! You won!"
         else
           make_guess
@@ -65,11 +70,11 @@ class Hangman
 
 
   def begin
-    puts "New game started... your clue is: #{ @word.first.size } letters long"
+    puts "New game started... your clue is: #{ @word.size } letters long"
     puts "To exit the game at any time, type 'exit'"
     print_teaser
 
-    puts "your clue is: #{ @word.last }"
+    puts "your clue is: #{ @word[0] }"
     make_guess
   end
 
