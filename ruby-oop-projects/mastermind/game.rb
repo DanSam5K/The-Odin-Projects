5a02423b193a05c9c173350932d5d0b35c740cfd
Daniel Samuel <dansam5000@gmail.com>
2022-11-07 21:40:10 +0100

    calculate score and make guess function

--- ruby-oop-projects/mastermind/game.rb
+++ ruby-oop-projects/mastermind/game.rb
@@ -17,3 +17,40 @@ def play
   end
 end
 
+def calculate_score(guess, answer)
+  score = ""
+  wrong_guess_pegs, wrong_answer_pegs = [], []
+  pegs_pairs = guess.chars.zip(answer.chars)
+
+  peg_pairs.each do |guess_peg, answer_peg|
+    if guess_peg == answer_peg
+      score << "B"
+    else
+      wrong_guess_pegs << guess_peg
+      wrong_answer_pegs << answer_peg
+    end
+  end
+
+  wrong_guess_pegs.each do |guess_peg|
+    if wrong_answer_pegs.include?(guess_peg)
+      wrong_answer_pegs.delete_at(wrong_answer_pegs.index(guess_peg))
+      score << "W"
+    end
+  end
+
+  score
+end
+
+def make_guess
+  if @guesses > 0
+    puts "Score: #{@score}"
+  else
+    puts "Guess the code. You have 10 guesses."
+  end
+
+  puts "Guesses Remaining: #{10 - @guesses}"
+  print "Guess: "
+  gets.chomp
+end
+
+play
