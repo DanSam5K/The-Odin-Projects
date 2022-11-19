ba1b159ade9dedb744cf12bea1299a8f6bef794c
Daniel Samuel <dansam5000@gmail.com>
2022-11-19 03:11:51 +0100

    recursion to solve parlindrome

--- algorithms/recursive_methods.rb
+++ algorithms/recursive_methods.rb
@@ -11,3 +11,42 @@ puts "Heaviest rock is: #{max_rock}"
 
 ## or with inject
 puts "Heaviest rock is: #{rocks.inject{|max_rock, rock| max_rock > rock ? max_rock : rock}}"
+
+def rock_judger(rocks_arr)    
+    if rocks_arr.length <= 2  # the base case
+      a = rocks_arr[0]
+      b = rocks_arr[-1]
+    else
+      a = rock_judger(rocks_arr.slice!(0,rocks_arr.length/2))
+      b = rock_judger(rocks_arr)
+    end    
+    
+    return a > b ? a : b
+end
+
+ 
+rocks = 30.times.map{rand(200) + 1}
+puts rocks.join(', ')
+puts "Heaviest rock is: #{rock_judger(rocks)}"
+
+def rock_judger(r_arr)     
+    count = r_arr.length
+    a,b =  count <= 2 ? [r_arr[0], r_arr[-1]] : [rock_judger(r_arr.pop(count/2)), rock_judger(r_arr)]
+    return a > b ? a : b
+end
+
+def palindrome(string)
+  if string.length < 2
+    true
+  else
+    if string[0] == string[-1]
+      palindrome(string[1..-2])
+    else
+      false
+    end
+  end
+end
+
+puts palindrome("I")
+
+
