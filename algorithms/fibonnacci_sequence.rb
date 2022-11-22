7273b47b583c8fb0a4662032f767298afb2af1ea
Daniel Samuel <dansam5000@gmail.com>
2022-11-22 21:32:37 +0100

    recursively implement #fibs

--- algorithms/fibonnacci_sequence.rb
+++ algorithms/fibonnacci_sequence.rb
@@ -1,12 +1,22 @@
-def fibs
-  a, b = 0, 1
-  while true
-    yield a
-    a, b = b, a + b
+# def fibs
+#   a, b = 0, 1
+#   while true
+#     yield a
+#     a, b = b, a + b
+#   end
+# end
+
+# fibs do |f|
+#   puts f
+#   break if f > 8
+# end
+
+def fibs(n)
+  if n < 2
+    n
+  else
+    fibs(n-1) + fibs(n-2)
   end
 end
 
-fibs do |f|
-  puts f
-  break if f > 8
-end
+fibs(10)
