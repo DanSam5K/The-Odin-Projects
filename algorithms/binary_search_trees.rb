9545a3aef7ecf86c66441a230c415613b25f6f04
Daniel Samuel <dansam5000@gmail.com>
2022-11-26 03:08:43 +0100

    add #pretty print method and #build_three method

--- algorithms/binary_search_trees.rb
+++ algorithms/binary_search_trees.rb
@@ -17,4 +17,26 @@ class Tree
     @root = build_tree(array)
   end
 
+  def build_tree(array)
+    return nil if array.empty?
+
+    array = array.uniq.sort
+    mid = array.length / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array[0...mid])
+    root.right = build_tree(array[mid + 1..-1])
+    root
+  end
+
+  def pretty_print(node = root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│ ' : ' '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? ' ' : '│ '}", true) if node.left
+  end
+
 end
+
+tree = Tree.new([1, 7, 4, 23, 8, 9, 3, 5, 6, 2, 1, 7, 4, 23, 8, 9, 3, 5, 6, 2])
+p tree.build_tree([1, 7, 4, 23, 8, 9, 3, 5, 6, 2, 1, 7, 4, 23, 8, 9, 3, 5, 6, 2])
+
+tree.pretty_print
