aef9a32ee0a7d0866c6386cc17bb995632d3161a
Daniel Samuel <dansam5000@gmail.com>
2022-11-28 02:47:28 +0100

    add Board class #knight_moves, #possible_moves and valid_moves

--- algorithms/knights_travails.rb
+++ algorithms/knights_travails.rb
@@ -25,3 +25,43 @@ class Knight
     path.reverse
   end
 end
+
+
+class Board
+  attr_accessor :board
+
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
+  end
+
+  def knight_moves(start, finish)
+    knight = Knight.new(start)
+    queue = [knight]
+    until queue.empty?
+      current = queue.shift
+      return current.find_path if current.position == finish
+      current.children = possible_moves(current.position)
+      current.children.each do |child|
+        child.parent = current
+        queue << child
+      end
+    end
+  end
+
+  def possible_moves(position)
+    moves = []
+    moves << [position[0] + 2, position[1] + 1]
+    moves << [position[0] + 2, position[1] - 1]
+    moves << [position[0] - 2, position[1] + 1]
+    moves << [position[0] - 2, position[1] - 1]
+    moves << [position[0] + 1, position[1] + 2]
+    moves << [position[0] + 1, position[1] - 2]
+    moves << [position[0] - 1, position[1] + 2]
+    moves << [position[0] - 1, position[1] - 2]
+    moves.select { |move| valid_move?(move) }.map { |move| Knight.new(move) }
+  end
+
+  def valid_move?(move)
+    move[0].between?(0, 7) && move[1].between?(0, 7)
+  end
+end
