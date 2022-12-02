f25c52d073fee027c2099e0006ffb0195a285f14
Daniel Samuel <dansam5000@gmail.com>
2022-12-02 11:55:05 +0100

    Refactor test to use subject instance

--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -1,37 +1,33 @@
-require_relative '../lib/calculator'
+require './lib/calculator'
 
 
 describe Calculator do
   describe "#add" do
     it "returns the sum of two numbers" do
-      calculator = Calculator.new
-      expect(calculator.add(5, 2)).to eq(7)
+      expect(subject.add(5, 2)).to eq(7)
     end
 
     it "returns the sum of more than two numbers" do
-      calculator = Calculator.new
-      expect(calculator.add(5, 2, 3)).to eq(10)
+      expect(subject.add(5, 2, 3)).to eq(10)
     end
   end
 
   describe "#subtract" do
     it "returns the difference of two numbers" do
-      calculator = Calculator.new
-      expect(calculator.subtract(5, 2)).to eq(3)
+      expect(subject.subtract(5, 2)).to eq(3)
     end
   end
 
   describe "#multiply" do
     it "returns the product of two numbers" do
-      calculator = Calculator.new
-      expect(calculator.multiply(5, 2)).to eq(10)
+      # subject = subject.new
+      expect(subject.multiply(5, 2)).to eq(10)
     end
   end
 
   describe "#divide" do
     it "returns the quotient of two numbers" do
-      calculator = Calculator.new
-      expect(calculator.divide(6, 2)).to eq(3)
+      expect(subject.divide(6, 2)).to eq(3)
     end
   end
 
